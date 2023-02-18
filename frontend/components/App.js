1509580762158019959c51d649cf99842f08518e
Sabrina <Smg21022@gmail.com>
2023-02-18 16:52:21 -0700

    Got Basic Layout Built

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task:'Finish Module 19',
+          id:1234,
+          completed: false
+        },
+        {
+          task:'Vacuum Floors',
+          id:2345,
+          completed:false
+        },
+        {
+          task:'Put Laundry Away',
+          id:3456,
+          completed:false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        Todo App
+        <h1>TODOS</h1>
+        <ul>
+          {
+            todos.map(todo=>{
+              return (<li>{todo.task} {todo.completed?<span>- completed</span> : <span></span>}</li>)
+            })
+          }
+        </ul>
+
+        <form>
+          <input />
+          <button>Add a Todo</button>
+          <button>Clear a Todo</button>
+        </form>
       </div>
     )
   }
 }
+ 
