3aa1428acfec12759404702736a70d37e3c19a1c
Sabrina <Smg21022@gmail.com>
2023-02-18 17:46:18 -0700

    Moved Form to Form Component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodoList from './TodoList';
+import Form from './Form';
 
 
 export default class App extends React.Component {
@@ -31,13 +32,10 @@ export default class App extends React.Component {
       <div>
         <h1>TODOS</h1>
         <TodoList todos={todos}/>
+        <Form />
 
-
-        <form>
-          <input />
-          <button>Add a Todo</button>
+       
           <button>Clear a Todo</button>
-        </form>
       </div>
     )
   }
